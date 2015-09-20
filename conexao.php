f70e4cf6afd20c473a78bba8d7a6db9f6da252c5
LeonardoCedrola <leozinmxm@gmail.com>
2015-09-19 23:37:23 -0300

    HTML feito #1
    Signed-off-by: LeonardoCedrola <leozinmxm@gmail.com>

---
+++ conexao.php
@@ -0,0 +1,43 @@
+<?php
+
+// Define os valores para as variáveis de conexão com o Banco de Dados
+#FileName="Connection_php_mysql.htm"
+		#Type="MYSQL"
+		#HTTP = "true"
+		$hostname = ""; // servidor local
+		$database = ""; // nome que eu coloquei no BD
+		$username = ""; // Login admin
+		$password = ""; // senha
+
+
+//$servidor = 'localhost';
+//$usuario = 'root';
+//$senha = '';
+//$banco = 'banco_modular';
+
+// Realiza a conexão com o banco de dados. A conexão fica armazenada em uma variável.
+/*
+Repare que o Mysqli já faz a seleção de um Banco de Dados específico dentro do "Servidor de Banco de Dados (MySQL)" na primeira 
+função 'mysqli_connect', ou seja, não é feita a conexão para depois selecionar o Bando de Dados com outra função
+A partir desse momento, todas as consultas, inserções, alterações e exclusões realizadas serão
+feitas SOMENTE no Banco de Dados selecionado
+*/
+$conn = new mysqli($hostname, $username, $password, $database);
+//$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+if ( mysqli_connect_error() ) {
+    $logMessage = 'MySQL Error: ' . mysqli_connect_error();
+	echo  'MySQL Error: ' . mysqli_connect_error();
+    // Call your logger here.
+    die('Could not connect to the database');
+}
+
+							/* 
+							
+							nas HTML que precisar usar banco de dados, colocar 
+								
+								<?php
+									require 'conexao.php';
+								?>  
+							*/
+?>
